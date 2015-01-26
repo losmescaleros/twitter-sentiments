d51157081059446f70fa730b70ac31e157d09aba
Mitchell <losmescaleros@gmail.com>
2015-01-26 18:25:00 -0500

    Implemented Twitter sentiment analysis server and client code

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+var credentials = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY, 
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET
+};
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -16,9 +23,50 @@ server.listen(port);
 console.log("Socket.io server listening on 127.0.0.1:" + port);
 
 var sio = require('socket.io').listen(server);
+var twitter = require('twitter');
+
+var t = new twitter(credentials);
+console.log("Connecting to Twitter stream...");
+
+var keywords = ['love', 'hate'];
+var loveCount = 0;
+var hateCount = 0;
+var total = 0;
 
 sio.sockets.on('connection', function(socket){
   console.log('Web client connected');
+  t.stream('statuses/filter', {track: keywords.join(',')}, function(stream){
+    stream.on('data', function(tweet){
+      if(tweet.text !== undefined){
+        var text = tweet.text.toLowerCase();
+        var shouldPrint = false;
+        if(text.indexOf(keywords[0]) > -1){
+          loveCount++;      
+          shouldPrint = true;
+        } 
+        if(text.indexOf(keywords[1]) > -1){
+          hateCount++;
+          shouldPrint = true;
+        }
+        if(shouldPrint){
+          total++;
+          var data = {
+            tweet: tweet.text,
+            tweeter: tweet.user.screen_name,
+            love: Math.round(100 * loveCount / total),
+            hate: Math.round(100 * hateCount / total),
+            total: total
+          };
+          socket.volatile.emit('tweet', data);
+          console.log(tweet.text);
+        }
+      }
+    });
+
+    stream.on('error', function(error, code){
+      console.log("ERROR " + code + ": " + error);
+    });
+  });
   
   socket.on('disconnect', function(){
     console.log('Web client disconnected');
--- public/javascripts/client.socket.io.js
+++ public/javascripts/client.socket.io.js
@@ -1,4 +1,21 @@
-var server_name = 'http://127.0.0.1:3000';
-var socket = io.connect(server_name);
+$(document).ready(function(){
 
-console.log('Connecting to server ' + server_name);
+  var server_name = 'http://127.0.0.1:3000';
+  var socket = io.connect(server_name);
+
+  console.log('Connecting to server ' + server_name);
+ 
+  var $tweetList = $("#tweets");
+  var $love = $("#love");
+  var $hate = $("#hate");
+  var $total = $("#total");
+
+  socket.on('tweet', function(data){
+    $tweetList.prepend('<li>' + 
+      data.tweeter + ': ' + 
+      data.tweet + '</li>');
+    $love.text(data.love + '%');
+    $hate.text(data.hate + '%');
+    $total.text(data.total);
+  });
+});
